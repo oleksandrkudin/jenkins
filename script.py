cf5978fc6103c133da9c1a8c623092695225e843
oleksandrkudin <oleksandr.kudin@gmail.com>
2019-02-26 10:13:55 +0200

    Add output message to test commit from PC

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
 from __future__ import print_function
 print('This is first output from script.py')
 print('This is job run is invoked automaticaly by Github after commit!!!')
+print('This change and commit from PC. Job run must be invoked automaticaly by Github after commit!!!')
